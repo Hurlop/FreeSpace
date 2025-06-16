328eddf8e7ea62673a51f04ea46318a81b03fe2f
Hurlop <daniel99020602844@gmail.com>
2025-06-16 18:48:55 -0500

    updated the way users are created as one field was removed

--- src/services/postCreateUser.js
+++ src/services/postCreateUser.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-export async function postCreateUser(email,password,cellphone,gender,first_name,last_name) {
+export async function postCreateUser(email,password,cellphone,first_name,last_name) {
     const REGISTER_URL = 'http://localhost:3000/users'
     try {
-        const response = await axios.post(REGISTER_URL, {email,password,cellphone,gender,first_name,last_name})
+        const response = await axios.post(REGISTER_URL, {email,password,cellphone,first_name,last_name})
         console.log(response)
         return response
     } catch (error) {
